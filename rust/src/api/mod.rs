34793324bbfd7d7e30fafd2e11d09447d1f632a1
ignalina <rickard@ignalina.dk>
2022-10-25 01:12:56 +0200

    make public

--- rust/src/api/mod.rs
+++ rust/src/api/mod.rs
@@ -19,7 +19,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 use arrow2::array::Array;
 
 
-trait OneShot {
+pub trait OneShot {
     fn set_lib(&self) -> usize;
     fn from_chunks(&self, arrays: Vec<Box<dyn Array>>) -> usize;
 
