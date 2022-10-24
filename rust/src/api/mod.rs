2b972e5a98b4d189199f4b58909f394958973435
ignalina <rickard@ignalina.dk>
2022-10-24 18:53:11 +0200

    first try,dont look at me!

--- rust/src/api/mod.rs
+++ rust/src/api/mod.rs
@@ -1,23 +1,59 @@
+/*
+MIT No Attribution
+
+Copyright 2022 Wilhelm Ågren & Rickard Ernst Björn Lundin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this
+software and associated documentation files (the "Software"), to deal in the Software
+without restriction, including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+use arrow2::array::Array;
+use chrono::offset::Local;
+
+macro_rules! info {
+    ($($arg:tt)*) => {
+        println!(
+            "{} [INFO] [Rust]\t{}",
+            Local::now().format("[%Y-%m-%d %H:%M:%S]"),
+            format!($($arg)*)
+        );
+    }
+}
 
 trait OneShot {
-    fn set_lib(&self) -> f32;
-    fn from_chunks(&self) -> f32;
-
+    fn set_lib(&self) -> usize;
+    fn from_chunks(&self, arrays: Vec<Box<dyn Array>>) -> usize;
 
 }
 
-struct Bridge {
+
+struct DummyBackend {
     ddlname: String,
 }
 
-impl OneShot for Bridge {
-    fn set_lib(&self) -> f32 {
-        1 as f32
-//        self.radius.powi(2) * std::f32::consts::PI
+impl OneShot for DummyBackend {
+    fn set_lib(&self) -> usize {
+        1
+
     }
 
-    fn from_chunks(&self) -> f32 {
+    fn from_chunks(&self, arrays: Vec<Box<dyn Array>>) -> usize {
 
-        todo!()
+        info!("Hello! Reading the rust pointers now.");
+
+        for (i, array) in arrays.iter().enumerate() {
+            info!("array{}: {:?}", i + 1, array);
+        }
+
+        arrays.len()
     }
 }
